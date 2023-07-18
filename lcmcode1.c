d9dfcc84a81eaaccebba6ade9197d8bb534d3eeb
UDAYXXX <139609275+UDAYXXX@users.noreply.github.com>
2023-07-18 12:06:56 +0530

    Add files via upload

---
+++ lcmcode1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int n1,n2;
+    printf("enter a number");
+    scanf("%d",&n1);
+    printf("enter a number");
+    scanf("%d",&n2);
+    
+    for(int i = 1;i<11;i++){
+        
+        int lcm1;
+        int lcm2;
+        lcm1 = n1*i;
+        lcm2 = n2*i;
+    }
+    int lcm1;
+    int lcm2;
+    int result= lcm1=lcm2;
+    printf("%d",result);
+    
+
+    return 0;
+}
